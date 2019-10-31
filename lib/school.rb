2dac25e0c7bf6e2c0b1425485c3b384149af730c
Will Drougas <wdrougas@gmail.com>
2019-10-31 17:12:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,48 @@
-# code here!
+require 'pry'
+
+class School
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, student_grade)
+    # Lines 15 and 16 are the same
+    roster[student_grade] = roster[student_grade] || []
+    roster[student_grade] ||= []
+    roster[student_grade] << student_name
+    
+    
+    
+    
+    
+    
+    
+    # # If roster[student_grade] does not exist, create it and set to an empty array
+    # if roster[student_grade] == nil
+    #   roster[student_grade] = []
+    # # If roster[student_grade] does exist, don't change it
+    # elsif roster[student_grade] != nil
+    #   roster[student_grade]
+    # end
+    
+  end
+  
+  
+  
+  def grade(student_grade)
+    roster[student_grade]
+  end
+  
+  def sort
+    result = @roster.each do |student_grade, student_name|
+      student_name.sort!
+    end
+  end
+    
+  
+end
+
